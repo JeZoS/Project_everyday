177e693447b099caf82b55878a91a7bc20207bae
JeZoS <prabhat262111@gmail.com>
2020-12-18 21:06:09 +0530

    49

--- 49_todos/script.js
+++ 49_todos/script.js
@@ -2,6 +2,12 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUl = document.getElementById("todos");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   addTodo();
@@ -20,17 +26,36 @@ function addTodo(todo) {
     }
     todoEl.innerText = todoText;
 
-    todoEl.addEventListener("click", () =>
-      todoEl.classList.toggle("completed")
-    );
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      updateLS();
+    });
 
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
       todoEl.remove();
+      updateLS();
     });
 
     todosUl.appendChild(todoEl);
 
     input.value = "";
+
+    updateLS();
   }
+}
+
+function updateLS() {
+  todosEl = document.querySelectorAll("li");
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+  //console.log(todos);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
